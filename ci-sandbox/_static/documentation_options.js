48d618dd2d490bfa320c5c40243f8176df1bcbaf
jerinphilip <jerinphilip@live.in>
2021-05-24 17:16:37 +0000

    Deploying to gh-pages from @ browsermt/bergamot-translator@c38cf9fe928a1fa88c6bf0dc3112b3ac04719062 🚀

--- ci-sandbox/_static/documentation_options.js
+++ ci-sandbox/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '&#34;ci-sandbox&#34; 2021-05-24',
+    VERSION: 'ci-sandbox 2021-05-24',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
