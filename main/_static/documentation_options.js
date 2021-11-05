e9132908a935a74c050da63c08be4b8cccc71cee
kpu <kpu@users.noreply.github.com>
2021-11-05 16:47:22 +0000

    Deploying to gh-pages from @ browsermt/bergamot-translator@fa4efb483ba4f5f4e3ac98bc8c3f14b2e87541f1 🚀

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'main 2021-11-03',
+    VERSION: 'main 2021-11-05',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
