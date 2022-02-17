785b79c4a1b5237bf2ac55c31100a742e28a86eb
abhi-agg <66322306+abhi-agg@users.noreply.github.com>
2022-02-17 13:40:35 +0000

    Deploying to gh-pages from @ browsermt/bergamot-translator@2844cedb0dc8b67df5b656e61b69e194a6658501 🚀

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'main 2022-02-15',
+    VERSION: 'main 2022-02-17',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
