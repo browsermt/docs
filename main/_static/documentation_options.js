5c8bd20c071146b0f43e041aa491e285dc51f863
jerinphilip <jerinphilip@live.in>
2022-02-23 13:30:57 +0000

    Deploying to gh-pages from @ browsermt/bergamot-translator@96b0f82343ab3ac4009aba8ecde706b6dca189d8 🚀

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'main 2022-02-22',
+    VERSION: 'main 2022-02-23',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
