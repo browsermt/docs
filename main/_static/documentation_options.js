ce3861acd4a41dcb5c3bff5fd372317736162dd7
XapaJIaMnu <nheart@gmail.com>
2023-08-17 07:16:43 +0000

    Deploying to gh-pages from @ browsermt/bergamot-translator@db3826266d11e611f9a96ab36a2deb84c4938697 🚀

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'main 2023-08-16',
+    VERSION: 'main 2023-08-17',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
